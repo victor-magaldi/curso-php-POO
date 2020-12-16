fba6b3db5567d25edc71c8ef2a6857d5e60d725d
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2020-12-15 21:13:14 -0300

    abstracao parte 01

---
+++ getters-e-setters.php
@@ -0,0 +1,47 @@
+<?php
+class Funcionario
+{
+    //atributos
+    public $nome = null;
+    public $telefone = null;
+    public $numFilhos = null;
+
+    //getter e setters
+    function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+    function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+    }
+    function setNumFilhos($numFilhos)
+    {
+        $this->numFilhos = $numFilhos;
+    }
+    function getNome()
+    {
+        return $this->nome;
+    }
+    function getTelefone()
+    {
+        return $this->telefone;
+    }
+    function getNumFilhos()
+    {
+        return $this->numFilhos;
+    }
+
+    //metodos 
+    function resumirCardfunc()
+    {
+        return "$this->nome possui $this->numFilhos esse é o resumo do card";
+    }
+    function modificaNumFilhos($num)
+    {
+        $this->numFilhos = $num;
+    }
+}
+$y = new Funcionario();
+echo $y->modificaNumFilhos(18);
+echo $y->resumirCardfunc();
--- poo-pilar-abstracao.php
+++ poo-pilar-abstracao.php
@@ -2,15 +2,20 @@
 class Funcionario
 {
     //atributos
-    public $nome = null;
-    public $telefone = null;
-    public $numFilhos = null;
+    public $nome = "josé";
+    public $telefone = "11 25252525";
+    public $numFilhos = 2;
 
     //metodos 
     function resumirCardfunc()
     {
-        return "esse é o resumo do card";
+        return "$this->nome possui $this->numFilhos esse é o resumo do card";
+    }
+    function modificaNumFilhos($num)
+    {
+        $this->numFilhos = $num;
     }
 }
 $y = new Funcionario();
+echo $y->modificaNumFilhos(18);
 echo $y->resumirCardfunc();
