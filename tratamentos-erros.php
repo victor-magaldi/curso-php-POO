069ceabcf4b8d8253b9a412bee5d37321d9b2a04
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-09 18:46:54 -0300

    criando Exception

--- tratamentos-erros.php
+++ tratamentos-erros.php
@@ -2,12 +2,26 @@
 
 try {
    echo '<h3>try</h3>';
-
    $sql = 'select * from cliente';
    mysqli_query($sql);
    // vai retornar um erro, pois ainda criei nenhum banco 
 } catch (Error $error) {
    echo 'catch' . $error;
 } finally {
-   echo '<h3>finally</h3>';
+   echo '<h3 class="teste">finally</h3>';
+}
+echo '<hr>';
+
+function dividir($x, $y) {
+   if ($y == 0) {
+      throw new Exception('é uma divisão por zero.');
+   }
+   $resultado = $x / $y;
+   return $resultado;
+};
+
+try {
+   echo dividir(5, 0) . "<br/>";
+} catch (Exception $e) {
+   echo 'Exceção capturada: ',  $e->getMessage(), "\n";
 }
